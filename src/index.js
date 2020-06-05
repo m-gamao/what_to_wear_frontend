0ebd353192ce52845d5209dcd40e5c60ef8dc8ff
gigi <mmgamao1@gmail.com>
2020-06-04 18:59:58 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 const endPoint = "http://localhost:3000/api/v1/outfits"
 
 document.addEventListener('DOMContentLoaded', () => {
-  // fetch and load outfits
+  // 1) fetch and load outfits
   getOutfits()
 
+  //document is the DOM
+  //look for an element in the html and save it to a variable const createOutfitForm
+  //#create-outfit-form is the ID of the element
+  //This form, you're going to click on a button and have JS listen to when that form is submitted
+  //and do something different from what we did in Rails. JS needs to know when that form is 
+  //submitted.
   const createOutfitForm = document.querySelector("#create-outfit-form")
 
+  //Event listener takes 2 functions - submit, and call back function that tells the app 
+  //what it should do.
+  //createFormHandler runs when the form is submitted and is then passed the event (e) 
+  //You have just submitted the form.
   createOutfitForm.addEventListener("submit", (e) => createFormHandler(e))
-
- // render edit form once button is clicked
-  const outfitContainer = document.querySelector('#outfit-container')
-  outfitContainer.addEventListener('click', e => {
-    const id = parseInt(e.target.dataset.id);
-    const outfit = Outfit.findById(id);
-    document.querySelector('#update-outfit').innerHTML = outfit.renderUpdateForm();
-
-// listen for the submit event of the edit form and handle the data
-  document.querySelector('#update-syllsbus').addEventListener('submit', e => updateFormHandler(e))
-
 })
+
+// don't worry about this yet
+ // render edit form once the button is clicked
+ //outfit-container is where the outfit data is going to go for the user to then see.
+//   const outfitContainer = document.querySelector('#outfit-container')
+//   outfitContainer.addEventListener('click', e => {
+//     const id = parseInt(e.target.dataset.id);
+//     const outfit = Outfit.findById(id);
+//     document.querySelector('#update-outfit').innerHTML = outfit.renderUpdateForm();
+
+// // listen for the submit event of the edit form and handle the data
+//   document.querySelector('#update-outfit').addEventListener('submit', e => updateFormHandler(e))
+
+// })
 
 function getOutfits() {
   fetch(endPoint)
@@ -36,12 +49,30 @@ function getOutfits() {
   })
 }
 
-// this function handles the inputs from the user
+//This function handles the inputs from the user
+//The createFormHandler runs whenever the form is submitted
+//const zipcodeInput and the querySelector #zipcode comes from the html.
+//Inside here is where you do your API Call.
 function createFormHandler(e) {
   e.preventDefault()
-  const zipcodeInput = document.querySelector('#input-zipcode').value
-  const weatherId = parseInt(document.querySelector('#weather').value)
-  postFetch(zipcodeInput, weatherId)
+  const zipcodeInput = document.querySelector('#zipcode').value
+
+ //use backticks to add the API CALL. String interpolate the zipcodeInput.
+   let URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcodeInput}&appid=529455d82ee4cf584b975c5961524222&units=imperial`
+
+   fetch(URL) 
+   .then(response => response.json())
+   .then(data => {
+    debugger
+     
+   })
+
+ 
+
+}
+
+
+
 
 
 // function createFormHandler(e) {
@@ -74,14 +105,13 @@ function postFetch(name, description, image_url, weather_id) {
     document.querySelector('#outfit-container').innerHTML += newOutfit.renderOutfitCard()
   })
 
-
+}
   function updateFormHandler(e) {
     e.preventDefault();
     const id = parseInt(e.target.dataset.id);
     const outfit = Outfit.findById(id);
-    const name = e.target.querySelector('#input-name').value;
+    const set = e.target.querySelector('#input-name').value;
     const description = e.target.querySelector('#input-description').value;
-    const image_url = e.target.querySelector('#input-url').value;
     const weather_id = parseInt(e.target.querySelector('#weather').value);
     patchOutfit(outfit, name, description, image_url, weather_id)
   }
@@ -100,4 +130,3 @@ function postFetch(name, description, image_url, weather_id) {
       // our backend responds with the updated outfit instance represented as JSON
       .then(updatedNote => console.log(updatedNote));
   }
-}
--- src/outfit.js
+++ src/outfit.js
@@ -3,17 +3,15 @@ class Outfit {
 
     constructor(outfit, outfitAttributes) {
       this.id = outfit.id
-      this.title = outfitAttributes.title
+      this.set = outfitAttributes.set
       this.description = outfitAttributes.description
-      this.image_url = outfitAttributes.image_url
-      this.category = outfitAttributes.category
+      this.weather = outfitAttributes.category
       Outfit.all.push(this)
     }
   
     renderOutfitCard() {
       return `
         <div data-id=${this.id}>
-          <img src=${this.image_url} height="200" width="250">
           <h3>${this.name}</h3>
           <p>${this.weather.name}</p>
           <button data-id=${this.id}>edit</button>
