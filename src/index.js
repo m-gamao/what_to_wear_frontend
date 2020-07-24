f798794c3c427c47ebd424dab14d56994601446a
gigi <mmgamao1@gmail.com>
2020-07-23 19:28:59 -0700

    adding new functionality - index.js, html, style.css

--- src/index.js
+++ src/index.js
@@ -1,43 +1,71 @@
-// 1) Event listener for the form:
+// STEP 1:
+  // Event listener for the form:
 document.addEventListener('DOMContentLoaded', () => {
   const cityForm = document.querySelector("#city-form");
   cityForm.addEventListener("submit", e => createFormHandler(e))
   const outfitsForm = document.querySelector("#outfits-form");
   outfitsForm.addEventListener("submit", e => postFetchOutfits(e))
-  // changed check boxes to <select>, so add event listener to it so that when it changes, it does ajax
+ 
   const citySelect = document.querySelector('#checkcity-id');
   citySelect.addEventListener('change', function(event){
     setOutfitDescription(this.value);
+
   })
 })
 
+// STEP 2: 
+  // This step will allow the user to see the original outfit listed in the description input field.
+   // take the cities_condition_id and make a fetch request to get the corresponding condition  
+  // use the condition to update the description in the new outfit form
+   // set the value of the description input to the outfit so the user can see what they are changing.
 function setOutfitDescription(citiesConditionId) {
-  // take the cities_condition_id and make a fetch request to get the corresponding condition
   fetch(`http://localhost:3000/api/v1/conditions/${citiesConditionId}`)
   .then(response => response.json())
   .then(condition => {
-    // use the condition to update the description in the new outfit form
     populateOutfitCondition(condition.outfit);
   })
 }
-
+​
 function populateOutfitCondition(outfit) {
-  // set the value of the description input to the outfit so the user can see what they are changing.
-  document.getElementById('description-input').value = outfit;
+   document.getElementById('description-input').value = outfit;
+}
+
+// STEP 3:
+  // Need to pull in the city names from the back end and then sort them dynamically in the JS.
+  // get the names of all the cities in the cities_conditions index
+  // render the cities in sorted order as per class file
+function getCityNames(e)
+  fetch(`http://localhost:3000/api/v1/conditions/${citiesConditionId}`)
+
+  
+function setOutfitDescription(citiesConditionId) {
+  fetch(`http://localhost:3000/api/v1/cities_conditions`)
+  .then(response => response.json())
+  .then(cities => {
+
+    const arrayCities = ["New York City", "Los Angeles", "Tokyo", "London", "Beijing", "Paris"]
+    cities.sort();
+    console.log(cities);
+
+    let newCities = new Cities(data);  // Data comes from the above fetch. Pass it to this method.
+    let newCitiesCard = newCities.renderCitiesCard();
+    document.getElementById('city-container').innerHTML = newCitiesCard;    ;
+  })
 }
 
 // 2) Form handler that handles the event. The city's options value is the id:
+    //get the value selected by the user.
+      //then we do the fetch, we'll send the id to the backend, 
+      //to get the condition related to it.
+      // so that params[:id] to be the id of the cities_condition (ln 68)
 function createFormHandler(e) {
     e.preventDefault()  //prevents page refresh.
-    //get the value selected by the user:
+
     const userInput = document.querySelector('#user-input').value;
     console.log(userInput); // Otherwise this function has no effect
 
-    //now we do the fetch, we'll send the id to the backend, 
-      //to get the condition related to it.
 
       fetch(`http://localhost:3000/api/v1/conditions/${userInput}`)  
-      // so that params[:id] to be the id of the cities_condition
       .then(response => response.json())
       .then(data => {
             let newWeather = new Weather(data);  // Data comes from the above fetch. Pass it to this method.
@@ -79,6 +107,8 @@ function CollapseForm() {
 //   document.addEventListener("submit", e => createOutfits(e))
 // }
 
+// key:value hashes in the bodyData:
+
 function postFetchOutfits(e) {
   e.preventDefault();
   // build my body object outside of my fetch
@@ -90,7 +120,7 @@ function postFetchOutfits(e) {
   }
 
   fetch('http://localhost:3000/api/v1/conditions/${checkcity-id}', {
-    // POST request
+    // Patch request
     method: "PATCH",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(bodyData)
